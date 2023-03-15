367259c5d7041ed2a0976317ea010c06a044b4af
Leslie Swan <42787368+PersonMeetup@users.noreply.github.com>
2023-03-15 09:21:27 -0600

    Push to prep GitHub Pages

--- scroll.js
+++ scroll.js
@@ -1,62 +1,29 @@
 window.onload = function () {
   // Declare global variables
-  var container = d3.select("#scroll");
-  var graphic = container.select(".scroll__graphic");
-  var text = container.select(".scroll__text");
-  var step = text.selectAll(".step");
-
-  function updateGraphic(index) {
-    const sel = container.select(`[data-step='${index}']`);
-    step.classed("step--active", (d, i) => i === index);
-
-    switch (index) {
-      case 0:
-        // Center image
-        break;
-      case 1:
-        // Hide boats
-        break;
-      case 2:
-        // Highlight China
-        break;
-      case 3:
-        // Show #shop, move shopping cart
-        break;
-      case 4:
-        // Show #dining, #computer at 50% progression
-        break;
-      case 5:
-        // Hide #dining, #computer
-        break;
-      case 6:
-        // Show boats, fewer in number
-        break;
-      case 7:
-        // Hide boats, highlight Russia and Ukraine
-        break;
-      default:
-      // Show boats on load
-    }
-  }
-
-  function progressGraphic(progress) {}
+  var graphic = document.getElementById("scroll__graphic");
 
   // On load, do this...
   function init() {
     enterView({
       selector: ".step",
-      offset: 0, // .275
+      offset: 0.275, // .275
       enter: (el) => {
-        const index = +d3.select(el).attr("data-step");
-        updateGraphic(index);
+        const index = el.getAttribute("data-step");
+        graphic.className = `scroll__graphic--step${index}`;
       },
       exit: (el) => {
-        let index = +d3.select(el).attr("data-step");
+        let index = el.getAttribute("data-step");
         index = Math.max(0, index - 1);
-        updateGraphic(index);
+        graphic.className = `scroll__graphic--step${index}`;
       },
+    });
+
+    enterView({
+      selector: "#scroll__graphic",
+      offset: 0,
       progress: (el, progress) => {
-        progressGraphic(progress);
+        const map = el.querySelector("#map");
+        map.style.height = progress * 100 + "%";
       },
     });
   }
